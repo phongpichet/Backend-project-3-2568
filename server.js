25b74787d4ad0f7e66efc82a24382612e4d5d8b1
phongpichet <luckyked20@email.com>
2025-03-11 14:34:59 +0700

    server

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use('/api/orders',orderRoutes);
 app.get('/', (req, res) => {
     res.send('Secondhand Shop API is running...');
 });
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
